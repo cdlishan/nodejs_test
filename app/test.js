0dab149462440223a09d161b683f2f15d651b83b
“cdlishan” <ls2002-10-08@163.com>
2017-10-09 16:45:24 +0800

    ts

---
+++ app/test.js
@@ -0,0 +1,17 @@
+
+//express_demo.js ??
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(8888, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("location: http://%s:%s", host, port)
+ 
+})
