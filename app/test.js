66714d685283a4e005e575489d1d36dfa7fc608a
“cdlishan” <ls2002-10-08@163.com>
2017-10-09 16:52:29 +0800

    fix bug#10010

--- app/test.js
+++ app/test.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.send('Test');
 })
  
 var server = app.listen(8888, function () {
